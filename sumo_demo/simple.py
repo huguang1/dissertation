107ab3cb4e107ae0c6d20d55fe2b5f16bea03d2f
huguang <1349291824@qq.com>
2023-05-09 09:30:04 +0100

    1

---
+++ sumo_demo/simple.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import subprocess
+import traci
+import sumolib
+
+
+def run():
+    # start sumo as a subprocess
+    sumoBinary = sumolib.checkBinary('sumo')
+    sumoProcess = subprocess.Popen([sumoBinary, "-c", "simple.sumocfg"], stdout=sys.stdout, stderr=sys.stderr)
+
+    # connect to sumo using traci
+    traci.init()
+    step = 0
+    while step < 3600:
+        traci.simulationStep()
+        step += 1
+
+    # close the connection to sumo
+    traci.close()
+
+
+if __name__ == '__main__':
+    run()
