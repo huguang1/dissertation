e4068d2c79d7dc8407b538a9b3e1bc109381a6bb
huguang <1349291824@qq.com>
2023-05-10 15:17:07 +0100

    1

--- sumo_demo3/tra.py
+++ sumo_demo3/tra.py
@@ -14,24 +14,6 @@ cache = {}
 step = 0
 while traci.simulation.getMinExpectedNumber() > 0:
     traci.simulationStep()
-    vehicles = traci.vehicle.getIDList()
-    step += 1
-    for i in range(len(vehicles)):
-        vehid = vehicles[i]
-        rembat = float(traci.vehicle.getParameter(vehid, "device.battery.actualBatteryCapacity"))
-        if (rembat < 600 and (vehid not in wait) and (vehid not in charge)):
-            target = traci.vehicle.getRoute(vehid)
-            target = target[len(target) - 1]
-            cache[vehid] = target
-            traci.vehicle.changeTarget(vehid, "E4")
-            wait.append(vehid)
-        if (vehid in wait):
-            wait.remove(vehid)
-            charge.append(vehid)
-        if (vehid in charge):
-            charge.remove(vehid)
-            traci.vehicle.changeTarget(vehid, cache[vehid])
-
-    time.sleep(0.001)
+    time.sleep(0.1)
 
 traci.close()
