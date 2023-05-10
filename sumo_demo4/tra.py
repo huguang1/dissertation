f8b2be8cdf97dcb9be8c2e0ff21ea55621d1b7d7
huguang <1349291824@qq.com>
2023-05-10 15:21:43 +0100

    1

--- sumo_demo4/tra.py
+++ sumo_demo4/tra.py
@@ -1,17 +1,10 @@
 # coding=utf-8
 import time
-
 import traci
 
 sumoCmd = ["sumo-gui", "-c", "con.sumocfg"]
 traci.start(sumoCmd)
 
-maxcap = 2000
-
-wait = []
-charge = []
-cache = {}
-step = 0
 while traci.simulation.getMinExpectedNumber() > 0:
     traci.simulationStep()
     time.sleep(0.01)
